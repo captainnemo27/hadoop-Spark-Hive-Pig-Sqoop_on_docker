e9361db6560459f1f1b77c495bbbe3a575614ccd
VanThangDao <vanthang.dao@officience.com>
2021-08-23 13:21:46 +0700

    push code set up sqoop

---
+++ config/sqoop-env.sh
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env bash
+
+echo "sqoop env ingggggg"
+
+-------------------------------------------
+#Set path to where bin/hadoop is available
+export HADOOP_COMMON_HOME=/usr/local/hadoop
+
+#Set path to where hadoop-*-core.jar is available
+export HADOOP_MAPRED_HOME=/usr/local/hadoop
+
+#Set the path to where bin/hive is available
+export HIVE_HOME=/use/local/hive
+-------------------------------------------
+# eof
