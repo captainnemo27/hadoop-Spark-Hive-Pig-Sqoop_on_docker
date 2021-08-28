7998aac81f592f2df48a1608a988c53fd55652ae
DaoVanThang <thangdao2018@gmail.com>
2021-08-28 10:13:28 +0700

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,8 +1,10 @@
 #After install project, create hadoop-network with command:
 docker network create --driver bridge hadoop-network --subnet=172.12.0.0/16
-
+# If you want to use a client connect accross docker , you can use network overlay:
+# docker network create -d overlay my-overlay
+# docker network create -d overlay --attachable my-attachable-overlay
 #Setup docker volume:
-docker volume create --name=myapp
+docker volume create --name=myhadoop
 
 #Then, you go into the project and run command to build the hadoop cluster:
 docker-compose build
